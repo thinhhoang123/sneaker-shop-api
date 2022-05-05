ac6b6604a384ba7eda06fb3d1dcf080b7288fa6f
thinh.hoang <thinh.hoang@thlgroup.vn>
2022-05-05 13:09:24 +0700

    init source

---
+++ README.md
@@ -0,0 +1,2 @@
+# fake-product-api-json-server
+Deploy at: https://fake-recipe-shopping-list.herokuapp.com/
---
+++ db.js
@@ -0,0 +1,7 @@
+const bestSeller = require("./data/bestSeller");
+const sneakerDetail = require("./data/sneakerDetail");
+
+module.exports = {
+  bestSeller,
+  sneakerDetail,
+}
